f75983fe206ed50881b472c50de0d4bbe6a4a725
Andrejchrcek <andrej.chrcek@gmail.com>
2025-10-14 19:28:32 +0200

    Update fpp_install.sh

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "FSEQDistributor installed successfully."
+sudo apt-get update
+sudo apt-get install -y python3-pandas python3-requests
+mkdir -p /home/fpp/media/plugins/fpp-FSEQDistributor/temp  # Temp folder pre súbory
+cp parse_fseq.py /home/fpp/media/plugins/fpp-FSEQDistributor/
+chmod +x /home/fpp/media/plugins/fpp-FSEQDistributor/parse_fseq.py
+echo "FSEQDistributor installed. Python deps: pandas, requests."
