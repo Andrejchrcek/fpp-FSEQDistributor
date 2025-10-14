83c3b5d55ea77b1e393e155682f6e875c39eb589
Andrejchrcek <andrej.chrcek@gmail.com>
2025-10-14 21:06:47 +0200

    Update fpp_install.sh

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
-# Zastaví skript, ak akýkoľvek príkaz zlyhá
 set -e
 
-# Inštalácia python knižnice
-pip3 install openpyxl
+# Vytvoríme log súbor
+LOGFILE="/home/fpp/media/logs/fpp_install_FSEQDistributor.log"
+echo "=== Inštalácia FSEQDistributor ===" > $LOGFILE
+echo "Dátum: $(date)" >> $LOGFILE
+
+echo "Inštalujem openpyxl..." | tee -a $LOGFILE
+pip3 install openpyxl 2>&1 | tee -a $LOGFILE
+
+echo "Inštalácia dokončená." | tee -a $LOGFILE
+echo "=== KONIEC ===" >> $LOGFILE
