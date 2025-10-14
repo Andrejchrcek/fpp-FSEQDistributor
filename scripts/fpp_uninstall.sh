43df2f59b34fff05474fcd7309e18c5868e53c85
Andrejchrcek <andrej.chrcek@gmail.com>
2025-10-14 18:30:55 +0200

    Update fpp_uninstall.sh

--- scripts/fpp_uninstall.sh
+++ scripts/fpp_uninstall.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# fpp-HomeAssistant uninstall script
-echo "Running fpp-HomeAssistant uninstall Script"
-
-BASEDIR=$(dirname $0)
-cd $BASEDIR
-cd ..
-make clean
-
+echo "FSEQDistributor uninstalled successfully."
